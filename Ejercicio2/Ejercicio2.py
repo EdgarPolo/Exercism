165b7cf67674a7756b6a2bda9f5c26935eba8d3e
Edgar Polo <edgarpolo70@gmail.com>
2021-04-05 20:09:30 -0500

    Ejercicio 2 Modificado

--- Ejercicio2/Ejercicio2.py
+++ Ejercicio2/Ejercicio2.py
@@ -1,4 +1,16 @@
 def distance(strand_a, strand_b, distancia=0):
+    """
+    Este Metodo Calcula La Distancia de Hamming de dos cadenas de ADN
+
+    Parameters
+    ----------
+    strand_a : cadena de adn
+    strand_b : cadena de adn
+
+    Returns
+    este metodo retorna un numero el cual es correspondiente a la distancia de
+    hamming de las dos cadenas
+    """
 
     if len(strand_a) != len(strand_b):
         raise ValueError("ValueError")
